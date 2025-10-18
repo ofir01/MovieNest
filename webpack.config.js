74a1e8ef4942c001e06cebaad808a8463e251d69
ofir01 <168498684+ofir01@users.noreply.github.com>
2025-10-19 02:27:47 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/ui/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
